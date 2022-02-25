8a6fb859f4fbd5df9666f654b612f840a0c99ab4
ubuntu <tigrxam@gmail.com>
2022-02-25 15:33:58 +0300

    task_4 - fix

--- lesson1/dz1/task4_encode_decode.py
+++ lesson1/dz1/task4_encode_decode.py
@@ -23,15 +23,18 @@ def get_str(word_bytes):
 
 def main():
     word = input('Введите слово: ')
-    print(word, type(word))
+    print(word, len(word), type(word))
+    print(f'Кодировка системы: {locale.getpreferredencoding()}')
 
     print("====================Кодирование====================")
     word_bytes = get_byte(word)
-    print(word_bytes, type(word_bytes))
+    print(word_bytes, len(word_bytes), type(word_bytes))
 
     print("====================Декодирование====================")
     word_byte_str = get_str(word_bytes)
-    print(word_byte_str, type(word_byte_str))
+    print(word_byte_str, len(word_byte_str), type(word_byte_str))
+
+    print(f'word == word_byte_str: {word == word_byte_str}')
 
 
 if __name__ == "__main__":
