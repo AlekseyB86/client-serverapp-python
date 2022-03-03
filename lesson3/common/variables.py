b51430b6071f14ffa4d954b89f68a7b0960004a5
ubuntu <tigrxam@gmail.com>
2022-03-03 17:20:21 +0300

    lesson3 - variables.py

---
+++ lesson3/common/variables.py
@@ -0,0 +1,25 @@
+"""Константы"""
+
+# Порт по умолчанию для сетевого взаимодействия
+DEFAULT_PORT = 7777
+
+# IP адрес по умолчанию для подключения клиента
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+
+# Мах очередь подключений
+MAX_CONNECTIONS = 5
+
+# кодировка проекта
+ENCODING = 'utf-8'
+
+# протокол JIM основные ключи
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+# прочие ключиб используемые в протоколе
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+RESPONDEFAULT_IP_ADDRESSE = 'respondefault_ip_addresse'
